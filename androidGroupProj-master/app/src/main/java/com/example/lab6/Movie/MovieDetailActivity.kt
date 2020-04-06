9fca196e1fc6cab406490fcb3904d29703d5190c
aigerim99k <aigerim.99k@gmail.com>
2020-04-06 18:51:11 +0500

    changed

--- androidGroupProj-master/app/src/main/java/com/example/lab6/Movie/MovieDetailActivity.kt
+++ androidGroupProj-master/app/src/main/java/com/example/lab6/Movie/MovieDetailActivity.kt
@@ -11,8 +11,6 @@ import com.example.lab6.MovieApi
 import com.example.lab6.R
 import com.example.lab6.RetrofitService
 import com.example.lab6.json.Result
-import kotlinx.android.synthetic.main.activity_movie_detail.*
-import kotlinx.android.synthetic.main.activityyy_main.*
 import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
